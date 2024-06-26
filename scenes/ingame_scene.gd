e3d360a7b0a86f38084bd3e091ddc71451eaa176
ImaokaHiroaki <wmoaiw@gmail.com>
2024-06-26 14:20:32 +0900

    シーソーへの衝突挙動実装

--- scenes/ingame_scene.gd
+++ scenes/ingame_scene.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var fade_overlay = %FadeOverlay
 @onready var pause_overlay = %PauseOverlay
+@onready var player = $Player
+@onready var seesawGround = $Seesaw/SeesawGround
 
 func _ready() -> void:
 	fade_overlay.visible = true
@@ -10,6 +12,10 @@ func _ready() -> void:
 		SaveGame.load_game(get_tree())
 	
 	pause_overlay.game_exited.connect(_save_game)
+	
+	# playerからシーソーへ与えるシグナル
+	player.player_seesaw_collided.connect(_on_player_seesaw_collided)
+
 
 func _input(event) -> void:
 	if event.is_action_pressed("pause") and not pause_overlay.visible:
@@ -20,3 +26,11 @@ func _input(event) -> void:
 		
 func _save_game() -> void:
 	SaveGame.save_game(get_tree())
+
+# シーソーへの衝突処理
+func _on_player_seesaw_collided(collided_position:Vector2, impulse:Vector2):
+	print("Observed1 got notify from Subject !!")
+	var seesawPosition = seesawGround.to_local(collided_position)
+	seesawGround.apply_impulse(seesawPosition, impulse)
+	print(collided_position)
+	print(seesawPosition)
--- scenes/main/Player.gd
+++ scenes/main/Player.gd
@@ -3,29 +3,42 @@ extends RigidBody2D
 @export var move_speed: float = 1000.0
 @export var move_speed_max = 100
 @export var jump_speed: float = 700.0
+@export var jump_impulse_strength: float = 10000.0
 
 @onready var move_right_force = Vector2(move_speed, 0)
 @onready var move_left_force = Vector2(-move_speed, 0)
 @onready var jump_force = Vector2(0, -jump_speed)
+@onready var collision_normal = Vector2(0, -1)
 
 @onready var ray_right_foot = $RightRayCast2D
 @onready var ray_left_foot = $LeftRayCast2D
 
+@onready var can_jump_buffer = false
+
+# 着地後にシーソーに与えるシグナル
+signal player_seesaw_collided(collided_position:Vector2, impulse:Vector2)
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	var can_jump = check_state()
+	var can_jump = check_jump()
+	if can_jump == true and can_jump_buffer == false:
+		var collision_point = get_global_position()
+		var impulse = collision_normal * jump_impulse_strength
+		player_seesaw_collided.emit(collision_point, impulse)
 	var force = input_process(can_jump)
 	self.apply_impulse(force, Vector2(0, 0))
+	can_jump_buffer = can_jump
 
-func check_state():
+# ジャンプ中か判定
+func check_jump():
 	if ray_right_foot.is_colliding() or ray_left_foot.is_colliding():
 		return true
 	else: 
 		return false
 
+# キー入力判定
 func input_process(can_jump:bool):
 	if can_jump:
 		if Input.is_action_pressed("ui_right") and self.linear_velocity.x < move_speed_max:
@@ -41,3 +54,4 @@ func input_process(can_jump:bool):
 		if Input.is_action_pressed("ui_left") and self.linear_velocity.x > -move_speed_max:
 			return Vector2(move_left_force.x/6, 0)
 	return Vector2(0, 0)
+
